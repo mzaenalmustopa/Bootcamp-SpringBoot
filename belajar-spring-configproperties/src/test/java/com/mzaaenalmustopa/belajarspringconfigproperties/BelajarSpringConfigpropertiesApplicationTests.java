74ea7e8dc4bc1a2fb13c9e497687bcfbc7730c14
Roni Purwanto <roni.purwanto@amartha.com>
2023-12-21 10:58:43 +0700

    spring config properties

---
+++ belajar-spring-configproperties/src/test/java/com/mzaaenalmustopa/belajarspringconfigproperties/BelajarSpringConfigpropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzaaenalmustopa.belajarspringconfigproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringConfigpropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
